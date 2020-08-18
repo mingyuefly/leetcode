39786046c917b1ad9b358e542a9ebb2457dd2494
mingyuefly <mingyuefly@qq.com>
2020-08-19 00:22:40 +0800

    242

---
+++ leetcode/hash table/valid anagram/Valid Anagram/Valid Anagram/main.cpp
@@ -0,0 +1,80 @@
+//
+//  main.cpp
+//  Valid Anagram
+//
+/**
+ Given two strings s and t , write a function to determine if t is an anagram of s.
+
+ Example 1:
+
+ Input: s = "anagram", t = "nagaram"
+ Output: true
+ Example 2:
+
+ Input: s = "rat", t = "car"
+ Output: false
+ Note:
+ You may assume the string contains only lowercase alphabets.
+ */
+//  Created by mingyue on 2020/8/18.
+//  Copyright © 2020 Gmingyue. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> sMap;
+        map<char, int> tMap;
+        unsigned long sLength = s.length();
+        unsigned long tLength = t.length();
+        if (s.length() == 0 && t.length() == 0) {
+            return true;
+        }
+        if (sLength != tLength) {
+            return false;
+        }
+        for (int i = 0; i < sLength; i++) {
+            char sa = s[i];
+            if (!sMap.count(sa)) {
+                sMap[sa] = 1;
+            } else {
+                int num = sMap[sa];
+                num++;
+                sMap[sa] = num;
+            }
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ta = t[i];
+            if (!tMap.count(ta)) {
+                tMap[ta] = 1;
+            } else {
+                int num = tMap[ta];
+                num++;
+                tMap[ta] = num;
+            }
+        }
+        return sMap == tMap;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    //string s = "anagram";
+    //string t = "nagaram";
+    string s = "aacc";
+    string t = "ccac";
+    Solution solution = Solution();
+    bool valid = solution.isAnagram(s, t);
+    if (valid) {
+        cout << "valid" << endl;
+    } else {
+        cout << "invalid" << endl;
+    }
+    return 0;
+}
--- leetcode/stack and queue/sliding window maxinum239/sliding window maxinum/sliding window maxinum/main.cpp
+++ leetcode/stack and queue/sliding window maxinum239/sliding window maxinum/sliding window maxinum/main.cpp
@@ -56,11 +56,9 @@ public:
             if (!window.empty() && window.front() == i - k) {
                 window.pop_front();
             }
-            
             while(!window.empty() && nums[i] > nums[window.back()]) {
                 window.pop_back();
             }
-            
             window.push_back(i);
             result.push_back(nums[window.front()]);
         }
